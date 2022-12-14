d4a03868dc177834cef71a473030bda519413ad9
Sean Wong <sean.uuong@gmail.com>
2022-12-14 11:00:04 -0800

    Complete Grocery List

--- app.js
+++ app.js
@@ -1,1 +1,30 @@
-// TODO
+const { useState } = React;
+
+const App = () => (
+  <div>
+    <h2>My Grocery List</h2>
+    <GroceryList items={['Banana', 'Apple']} />
+  </div>
+);
+
+const GroceryList = (props) => (
+  <ul>
+    {props.items.map((item) => (
+      <GroceryListItem item={item} />
+    ))}
+  </ul>
+);
+
+const GroceryListItem = (props) => {
+  const [isOver, setIsOver] = useState(false);
+  const style = {
+    fontWeight: isOver ? 'bold' : 'normal'
+  };
+
+  return (
+    <li style={style} onMouseEnter={() => setIsOver(!isOver)} onMouseLeave={() => setIsOver(!isOver)}>
+      {props.item}</li>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById("app"));
